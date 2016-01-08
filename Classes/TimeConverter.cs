8fb3d6df523338d419f546075b4f677d7d923505
unknown <mazda@aduki.co.uk>
2016-01-08 17:14:54 +0100

    Intial commit

---
+++ Classes/TimeConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClock
+{
+    public class TimeConverter : ITimeConverter
+    {
+        public string convertTime(string aTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
